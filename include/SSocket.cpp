1ec23e2c2d4f67a7e4db8ddfa39a7f62d671d7f9
Will <will.whitty.arbeit@gmail.com>
2015-09-01 13:24:41 +0300

    Fixed error codes, added valid() method to check socket status

--- include/SSocket.cpp
+++ include/SSocket.cpp
@@ -21,42 +21,42 @@ std::string SSocket::READ_BUFFER = "";
 SSocket::Callback SSocket::STREAM_FUNC = 0;
 
 size_t SSocket::WRITE_CALLBACK(void *contents, size_t size, size_t nmemb, void *userp) {
-  //Callback function used for writing data from the web directly into a string
-  ((std::string*) userp)->append((char *)contents, size * nmemb);
-  return size * nmemb;
+    //Callback function used for writing data from the web directly into a string
+    ((std::string*) userp)->append((char *)contents, size * nmemb);
+    return size * nmemb;
 }
 size_t SSocket::STREAM_CALLBACK(void *contents, size_t size, size_t nmemb, void *userp) {
-  //Callback function used for streaming callback
+    //Callback function used for streaming callback
 
-  //Return true from user function to exit stream prematurely
-  return ((SSocket::STREAM_FUNC)(contents, size, nmemb)) ? 0 : size * nmemb;
+    //Return true from user function to exit stream prematurely
+    return ((SSocket::STREAM_FUNC)(contents, size, nmemb)) ? 0 : size * nmemb;
 }
 size_t SSocket::FPUT_CALLBACK(void *contents, size_t size, size_t nmemb, void *outputFile) {
-  //Callback which appends data to a file
-  return fwrite(contents, size, nmemb, (FILE*) outputFile);
+    //Callback which appends data to a file
+    return fwrite(contents, size, nmemb, (FILE*) outputFile);
 }
 SSocket::SSocket() {
-//Construcrtor
-  //Initialize our error code
-  this->res  = (CURLcode) 0;
+    //Construcrtor
+    //Initialize our error code
+    this->res  = (CURLcode) 0;
   
-  //Setup our socket if first time
-  this->curl = curl_easy_init();
+    //Setup our socket if first time
+    this->curl = curl_easy_init();
   
-  //Maximum wait before timeout
-  this->option(CURLOPT_TIMEOUT, 10);
-  this->option(CURLOPT_WRITEDATA, &SSocket::READ_BUFFER);
-  this->option(CURLOPT_WRITEFUNCTION, SSocket::WRITE_CALLBACK);
+    //Maximum wait before timeout
+    this->option(CURLOPT_TIMEOUT, 10);
+    this->option(CURLOPT_WRITEDATA, &SSocket::READ_BUFFER);
+    this->option(CURLOPT_WRITEFUNCTION, SSocket::WRITE_CALLBACK);
 }
 
 SSocket::~SSocket() {
-//Destructor
-  if (this->curl != NULL)
-    curl_easy_cleanup(this->curl);
+    //Destructor
+    if (this->curl != NULL)
+        curl_easy_cleanup(this->curl);
 }
 
 CURL * SSocket::handle() {
-  return this->curl;
+    return this->curl;
 }
 
 ////
@@ -64,11 +64,16 @@ CURL * SSocket::handle() {
 // Error reporting and debugging
 //
 
-bool SSocket::check(CURLcode res) {
-  return ((this->res = res) == CURLE_OK) ? true : false;
+bool SSocket::valid() {
+    return (this->res == CURLE_OK) ? true : false;
+}
+bool SSocket::check(CURLcode newRes) {
+    return ((this->res = newRes) == CURLE_OK ? true : false);
 }
 const char * SSocket::error() {
-  return curl_easy_strerror(this->res);
+    std::string errorBuffer("[Error] ");
+    errorBuffer += curl_easy_strerror(this->res);
+    return errorBuffer.c_str();
 }
 
 ////
@@ -77,26 +82,26 @@ const char * SSocket::error() {
 //
 
 bool SSocket::option(CURLoption option, long param) {
-  return this->check(curl_easy_setopt(this->curl, option, param));
+    return this->check(curl_easy_setopt(this->curl, option, param));
 }
 bool SSocket::option(CURLoption option, void * param) {
-  return this->check(curl_easy_setopt(this->curl, option, param));
+    return this->check(curl_easy_setopt(this->curl, option, param));
 }
 bool SSocket::option(CURLoption option, const char * param) {
-  return this->check(curl_easy_setopt(this->curl, option, param));
+    return this->check(curl_easy_setopt(this->curl, option, param));
 }
 bool SSocket::option(CURLoption option, std::string * param) {
-  return this->check(curl_easy_setopt(this->curl, option, param));
+    return this->check(curl_easy_setopt(this->curl, option, param));
 }
 bool SSocket::option(CURLoption option, size_t (*param)(void *, size_t, size_t, void *)) {
-  return this->check(curl_easy_setopt(this->curl, option, param));
+    return this->check(curl_easy_setopt(this->curl, option, param));
 }
 
 void SSocket::debug(bool param) {
-  this->option(CURLOPT_VERBOSE, (param ? 1L : 0L));
+    this->option(CURLOPT_VERBOSE, (param ? 1L : 0L));
 }
 void SSocket::timeout(long time) {
-  this->option(CURLOPT_TIMEOUT, time);
+    this->option(CURLOPT_TIMEOUT, time);
 }
 
 ////
@@ -105,33 +110,33 @@ void SSocket::timeout(long time) {
 //
 
 std::string SSocket::build_query(const std::map<std::string, std::string> query) {
-   //Reuse our static string buffer
-  SSocket::READ_BUFFER = "";
+    //Reuse our static string buffer
+    SSocket::READ_BUFFER = "";
 
-  //Loop through the hash and add the keys to the buffer
-  for (const auto &it : query)
-    SSocket::READ_BUFFER += it.first + "=" + it.second + "&";
+    //Loop through the hash and add the keys to the buffer
+    for (const auto &it : query)
+        SSocket::READ_BUFFER += it.first + "=" + it.second + "&";
 
-  // Remove the trailing "&"
-  SSocket::READ_BUFFER.pop_back();
+    // Remove the trailing "&"
+    SSocket::READ_BUFFER.pop_back();
   
-  return SSocket::READ_BUFFER;
+    return SSocket::READ_BUFFER;
 }
 std::string SSocket::http_build_query(const std::map<std::string, std::string> query) {
-//Return a query for url get arguments to a server based php script
+    //Return a query for url get arguments to a server based php script
 
-  //Use the specified options to save the query as a string
-  //Ignore return value to reuse SSocket::READ_BUFFER
-  this->build_query(query);
+    //Use the specified options to save the query as a string
+    //Ignore return value to reuse SSocket::READ_BUFFER
+    this->build_query(query);
     
-  //Return the buffer
-  return (query.empty() ? "" : "?") + SSocket::READ_BUFFER;
+    //Return the buffer
+    return (query.empty() ? "" : "?") + SSocket::READ_BUFFER;
 }
 
 std::string SSocket::http_build_query(const std::string url, const std::map<std::string, std::string> query) {
-//Return a query for passing url get arguments to a server based php script
-  //Call internal query func and return concat
-  return url + this->http_build_query(query);
+    //Return a query for passing url get arguments to a server based php script
+    //Call internal query func and return concat
+    return url + this->http_build_query(query);
 }
 
 ////
@@ -140,26 +145,26 @@ std::string SSocket::http_build_query(const std::string url, const std::map<std:
 //
 
 void SSocket::configure(std::string url) {
-//Return or initialize this socket object
-  this->option(CURLOPT_URL, url.c_str());
+    //Return or initialize this socket object
+    this->option(CURLOPT_URL, url.c_str());
 }
 
 std::string SSocket::execute() {
-//Call curl_easy_perform and check for errors
+    //Call curl_easy_perform and check for errors
 
-  //Reinitialize our read buffer
-  SSocket::READ_BUFFER = "";
+    //Reinitialize our read buffer
+    SSocket::READ_BUFFER = "";
 
-  //If curl has failed somehow
-  if (! this->curl)
-    return "Curl initialization failure";
+    //If curl has failed somehow
+    if (! this->curl)
+        return "[Error] Curl initialization failure";
 
-  //Perform and check for errors
-  if (! this->check(curl_easy_perform(this->curl)))
-    return this->error();
+    //Perform and check for errors
+    if (! this->check(curl_easy_perform(this->curl)))
+        return this->error();
 
-  //Return the buffer
-  return SSocket::READ_BUFFER;
+    //Return the buffer
+    return SSocket::READ_BUFFER;
 }
 
 ////
@@ -168,17 +173,17 @@ std::string SSocket::execute() {
 //
 
 void SSocket::auth(int value) {
-  //Tell our socket to verify both the host and peer
-  this->auth_peer(value);
-  this->auth_host(value);
+    //Tell our socket to verify both the host and peer
+    this->auth_peer(value);
+    this->auth_host(value);
 }
 void SSocket::auth_peer(int value) {
-  //Tell our socket to verify the client or not
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, value);
+    //Tell our socket to verify the client or not
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, value);
 }
 void SSocket::auth_host(int value) {
-  //Tell our socket to verify the host or not
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, value);
+    //Tell our socket to verify the host or not
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, value);
 }
 
 ////
@@ -188,133 +193,133 @@ void SSocket::auth_host(int value) {
 
 std::string SSocket::get(const std::string & url,
                          const std::string & queryData) {
-//Perform a Simple get request
-  this->configure(url + (queryData.length() > 0 ? "?" + queryData : ""));
+    //Perform a Simple get request
+    this->configure(url + (queryData.length() > 0 ? "?" + queryData : ""));
 	
-  if (this->curl) {
-    //get request setup
-    this->option(CURLOPT_HTTPGET, 1);
-    this->option(CURLOPT_CUSTOMREQUEST, "GET");
-  }
+    if (this->curl) {
+        //get request setup
+        this->option(CURLOPT_HTTPGET, 1);
+        this->option(CURLOPT_CUSTOMREQUEST, "GET");
+    }
   
-  return this->execute();
+    return this->execute();
 }		
 
 std::string SSocket::get(const std::string & url,
                          const std::map<std::string, std::string> & queryData) {
-//Send a get request, using a std::map (Hashed vector) for the query data
-  return this->get(url, this->http_build_query(queryData));
+    //Send a get request, using a std::map (Hashed vector) for the query data
+    return this->get(url, this->http_build_query(queryData));
 }
 
 std::string SSocket::post(const std::string & url,
                           const std::string & postData) {
-//Send a post request using a simple std::string for the data 
-  this->configure(url); 
-  if (this->curl) {
-    //Tell curl we want to post
-    this->option(CURLOPT_POST, 1);				
-    this->option(CURLOPT_CUSTOMREQUEST, "POST");	  
-    //Concatenate body and post data
-    this->option(CURLOPT_POSTFIELDSIZE, postData.length());
-    this->option(CURLOPT_POSTFIELDS, postData.c_str());
-  }
-  return this->execute();
+    //Send a post request using a simple std::string for the data 
+    this->configure(url); 
+    if (this->curl) {
+        //Tell curl we want to post
+        this->option(CURLOPT_POST, 1);				
+        this->option(CURLOPT_CUSTOMREQUEST, "POST");	  
+        //Concatenate body and post data
+        this->option(CURLOPT_POSTFIELDSIZE, postData.length());
+        this->option(CURLOPT_POSTFIELDS, postData.c_str());
+    }
+    return this->execute();
 }
 
 std::string SSocket::post(const std::string & url,
                           const std::map<std::string, std::string> & queryData) {
-//Send a post request using a std::map (Hashed vector) for the input 
-  return this->post(url, this->http_build_query(queryData));
+    //Send a post request using a std::map (Hashed vector) for the input 
+    return this->post(url, this->http_build_query(queryData));
 }
 
 std::string SSocket::patch(const std::string & url,
                            const std::string & postData) {
-//Send a patch request using a simple std::string for the data
-  //Send a patch request 	
-  this->configure(url);
+    //Send a patch request using a simple std::string for the data
+    //Send a patch request 	
+    this->configure(url);
   
-  if (this->curl) {
-    //Tell curl we want to post
-    this->option(CURLOPT_POST, 1);
-    //patch request setup
-    this->option(CURLOPT_CUSTOMREQUEST, "PATCH");
-    //Concatenate body and post data
-    this->option(CURLOPT_POSTFIELDSIZE, postData.length());
-    this->option(CURLOPT_POSTFIELDS, postData.c_str());
-  }
-  return this->execute();
+    if (this->curl) {
+        //Tell curl we want to post
+        this->option(CURLOPT_POST, 1);
+        //patch request setup
+        this->option(CURLOPT_CUSTOMREQUEST, "PATCH");
+        //Concatenate body and post data
+        this->option(CURLOPT_POSTFIELDSIZE, postData.length());
+        this->option(CURLOPT_POSTFIELDS, postData.c_str());
+    }
+    return this->execute();
 }
 
 std::string SSocket::patch(const std::string & url,
                            const std::map<std::string, std::string> & queryData) {
-//Send a post request using a std::map (Hashed vector) for the input 
-  return this->patch(url, this->http_build_query(queryData));
+    //Send a post request using a std::map (Hashed vector) for the input 
+    return this->patch(url, this->http_build_query(queryData));
 }
 
 std::string SSocket::del(const std::string & url) {
-//Send a simple Delete request, using only a URL
-  //Send a DELETE request 
-  this->configure(url);
+    //Send a simple Delete request, using only a URL
+    //Send a DELETE request 
+    this->configure(url);
 
-  if (this->curl)
-    //DELETE request setup
-    this->option(CURLOPT_CUSTOMREQUEST, "DELETE");
+    if (this->curl)
+        //DELETE request setup
+        this->option(CURLOPT_CUSTOMREQUEST, "DELETE");
 	
-  return this->execute();
+    return this->execute();
 }
 void SSocket::stream(const std::string & url) {
 
-  //Url setup
-  this->configure(url);
-  //Start the stream, return true from (*callback) to exit
-  this->execute();
+    //Url setup
+    this->configure(url);
+    //Start the stream, return true from (*callback) to exit
+    this->execute();
 }
 void SSocket::stream(const std::string & url, SSocket::Callback callback) {
-  //Open a new curl stream to url, and call (*callback) for each data package
+    //Open a new curl stream to url, and call (*callback) for each data package
 
-  //Setup our streaming callback, which will in turn call (*callback)
-  this->option(CURLOPT_WRITEFUNCTION, SSocket::STREAM_CALLBACK);
+    //Setup our streaming callback, which will in turn call (*callback)
+    this->option(CURLOPT_WRITEFUNCTION, SSocket::STREAM_CALLBACK);
 
-  //Save our callback function
-  SSocket::STREAM_FUNC = callback;
+    //Save our callback function
+    SSocket::STREAM_FUNC = callback;
 
-  //Launch the stream
-  this->stream(url);
+    //Launch the stream
+    this->stream(url);
 
-  //Reset our curl callback to the write buffer
-  this->option(CURLOPT_WRITEFUNCTION, SSocket::WRITE_CALLBACK);
+    //Reset our curl callback to the write buffer
+    this->option(CURLOPT_WRITEFUNCTION, SSocket::WRITE_CALLBACK);
 
 }
 void SSocket::stream(const std::string & url, std::string & buffer) {
-//Setup our streaming callback, which will in turn call (*callback)
-  //Set our curl callback to the write buffer
-  this->option(CURLOPT_WRITEDATA, &buffer);
+    //Setup our streaming callback, which will in turn call (*callback)
+    //Set our curl callback to the write buffer
+    this->option(CURLOPT_WRITEDATA, &buffer);
   
-  //Launch the stream
-  this->stream(url);
+    //Launch the stream
+    this->stream(url);
 
-  //Reset our curl callback to the write buffer
-  this->option(CURLOPT_WRITEDATA, &SSocket::READ_BUFFER);
+    //Reset our curl callback to the write buffer
+    this->option(CURLOPT_WRITEDATA, &SSocket::READ_BUFFER);
 }
 void SSocket::stream(const std::string & url, const char * fileName, bool binary) {
-  FILE * file;
-  if (! (file = fopen(fileName, (binary ? "wb" : "w")))) {
-    printf("Could not open file; %s, for writing.\n", fileName);
-    return;
-  }
+    FILE * file;
+    if (! (file = fopen(fileName, (binary ? "wb" : "w")))) {
+        printf("Could not open file; %s, for writing.\n", fileName);
+        return;
+    }
   
-  if (binary)
-    this->option(CURLOPT_HTTP_TRANSFER_DECODING, 0L);
-  this->option(CURLOPT_WRITEFUNCTION, SSocket::FPUT_CALLBACK);
-  this->option(CURLOPT_WRITEDATA, file);
+    if (binary)
+        this->option(CURLOPT_HTTP_TRANSFER_DECODING, 0L);
+    this->option(CURLOPT_WRITEFUNCTION, SSocket::FPUT_CALLBACK);
+    this->option(CURLOPT_WRITEDATA, file);
   
-  //Launch the stream
-  this->stream(url);
+    //Launch the stream
+    this->stream(url);
 
-  //Reset our curl callback
-  this->option(CURLOPT_HTTP_TRANSFER_DECODING, 1L);
-  this->option(CURLOPT_WRITEDATA, &SSocket::READ_BUFFER);
-  this->option(CURLOPT_WRITEFUNCTION, SSocket::WRITE_CALLBACK);
+    //Reset our curl callback
+    this->option(CURLOPT_HTTP_TRANSFER_DECODING, 1L);
+    this->option(CURLOPT_WRITEDATA, &SSocket::READ_BUFFER);
+    this->option(CURLOPT_WRITEFUNCTION, SSocket::WRITE_CALLBACK);
 
-  fclose(file);
+    fclose(file);
 }
--- include/SSocket.hpp
+++ include/SSocket.hpp
@@ -29,61 +29,63 @@
 class SSocket {
   
 public:
-  // Type of streaming callback function 
-  typedef bool (*Callback)(void *, int, int);
+    // Type of streaming callback function 
+    typedef bool (*Callback)(void *, int, int);
 
-  SSocket();
-  ~SSocket();
+    SSocket();
+    ~SSocket();
 
-  CURL * handle();
-  bool check(CURLcode);
-  const char* error();
+    CURL * handle();
+    
+    bool valid();
+    bool check(CURLcode);
+    const char* error();
   
-  bool option(CURLoption, long);
-  bool option(CURLoption, void *);
-  bool option(CURLoption, const char *);
-  bool option(CURLoption, std::string *);
-  bool option(CURLoption, size_t (*)(void *, size_t, size_t, void *));
+    bool option(CURLoption, long);
+    bool option(CURLoption, void *);
+    bool option(CURLoption, const char *);
+    bool option(CURLoption, std::string *);
+    bool option(CURLoption, size_t (*)(void *, size_t, size_t, void *));
 
-  void debug(bool);
-  void timeout(long);
+    void debug(bool);
+    void timeout(long);
   
-  void auth(int);
-  void auth_peer(int);
-  void auth_host(int);
+    void auth(int);
+    void auth_peer(int);
+    void auth_host(int);
 
-  std::string del(const std::string &);
-  std::string get(const std::string &, const std::string & = "");
-  std::string post(const std::string &, const std::string & = "");
-  std::string patch(const std::string &, const std::string & = "");
+    std::string del(const std::string &);
+    std::string get(const std::string &, const std::string & = "");
+    std::string post(const std::string &, const std::string & = "");
+    std::string patch(const std::string &, const std::string & = "");
 
-  std::string get(const std::string &, const std::map<std::string, std::string> &);
-  std::string post(const std::string &, const std::map<std::string, std::string> &);
-  std::string patch(const std::string &, const std::map<std::string, std::string> &);
+    std::string get(const std::string &, const std::map<std::string, std::string> &);
+    std::string post(const std::string &, const std::map<std::string, std::string> &);
+    std::string patch(const std::string &, const std::map<std::string, std::string> &);
 
-  void stream(const std::string &, Callback);
-  void stream(const std::string &, std::string &);
-  void stream(const std::string &, const char *, bool = false);
+    void stream(const std::string &, Callback);
+    void stream(const std::string &, std::string &);
+    void stream(const std::string &, const char *, bool = false);
   
 protected:
-  CURL *	  curl;
-  CURLcode 	  res;
+    CURL *	  curl;
+    CURLcode 	  res;
 
-  std::string execute();
-  void configure(const std::string);
-  void stream(const std::string &);
-  // Map to string
-  std::string build_query(const std::map<std::string, std::string>);
+    std::string execute();
+    void configure(const std::string);
+    void stream(const std::string &);
+    // Map to string
+    std::string build_query(const std::map<std::string, std::string>);
 
-  // Map to HTTP query string
-  std::string http_build_query(const std::map<std::string, std::string>);
-  std::string http_build_query(const std::string, const std::map<std::string, std::string>);
+    // Map to HTTP query string
+    std::string http_build_query(const std::map<std::string, std::string>);
+    std::string http_build_query(const std::string, const std::map<std::string, std::string>);
 
-  static std::string READ_BUFFER;
-  static Callback    STREAM_FUNC;
-  static size_t FPUT_CALLBACK(void *, size_t, size_t, void *);
-  static size_t WRITE_CALLBACK(void *, size_t, size_t, void *);
-  static size_t STREAM_CALLBACK(void *, size_t, size_t, void *);
+    static std::string READ_BUFFER;
+    static Callback    STREAM_FUNC;
+    static size_t FPUT_CALLBACK(void *, size_t, size_t, void *);
+    static size_t WRITE_CALLBACK(void *, size_t, size_t, void *);
+    static size_t STREAM_CALLBACK(void *, size_t, size_t, void *);
 };
 
 #endif
